7ca3e1e0759fbbcba8c8ccd9db395dddc4487e67
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-11 21:56:04 +0000

    Rendering NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './chakra.theme';
-
+import NavigationBar from './components/NavigationBar';
 export default function App() {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	return <ChakraProvider theme={theme} />;
+	return (
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		<ChakraProvider theme={theme} resetCSS>
+			<NavigationBar />
+		</ChakraProvider>
+	);
 }
