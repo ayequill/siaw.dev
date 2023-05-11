972c3d04e6972f62597cecca8fa7d2cf71d5dd1d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-11 16:38:05 +0000

    ADD: Added theme file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './chakra.theme';
 
 export default function App() {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	return <ChakraProvider theme={theme} />;
 }
---
+++ src/chakra.theme.ts
@@ -0,0 +1,8 @@
+import {extendTheme} from "@chakra-ui/react";
+import "@fontsource/sen";
+
+export const theme = extendTheme({
+    fonts: {
+        body: 'Sen, sans-serif',
+    },
+})
