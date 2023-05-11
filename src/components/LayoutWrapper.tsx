ecfa380f40d5b3a3f89a1fe8e02f0fbdabf886b0
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-11 21:58:00 +0000

    ADD: Wrapper Component

---
+++ src/components/LayoutWrapper.tsx
@@ -0,0 +1,13 @@
+import React, {ReactNode} from "react";
+import {Box} from "@chakra-ui/react";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+export const LayoutWrapper = ({children}: LayoutProps) => {
+    return(
+        <Box ml='1.5rem' mr='1.5rem' maxW='767px'>
+            {children}
+        </Box>
+    )
+}
