1829e4541c68388c76716d08625e74b5ed80f855
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-12 06:40:12 +0000

    REFACTOR: Formatted code

--- src/chakra.theme.ts
+++ src/chakra.theme.ts
@@ -1,16 +1,30 @@
-import {extendTheme} from "@chakra-ui/react";
-import "@fontsource/sen";
+import { extendTheme } from '@chakra-ui/react';
+import '@fontsource/sen';
 
+const hue = 250;
 export const theme = extendTheme({
-    fonts: {
-        body: 'Sen, sans-serif',
-    },
-    global: {
-        body: {
-            position: 'relative',
-        },
-        li: {
-            listStyle: 'none',
-        }
-    }
-})
+	fonts: {
+		body: 'Sen, sans-serif',
+	},
+	colors: {
+		brand: {
+			primary: `hsl(${hue}, 69%, 61%)`,
+			secondary: `hsl(${hue}, 69%, 61%)`,
+		},
+	},
+	global: {
+		body: {
+			position: 'relative',
+		},
+		li: {
+			listStyle: 'none',
+		},
+	},
+	layerStyles: {
+		navLinkStyles: {
+			alignItems: 'center',
+			display: 'flex',
+			flexDir: 'column',
+		},
+	},
+});
