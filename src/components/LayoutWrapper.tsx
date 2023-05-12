9cfb0a07ce7b85ac02ca06d2c440115608a2baa7
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-12 06:38:40 +0000

    FIX: Fixes props

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -1,13 +1,13 @@
-import React, {ReactNode} from "react";
-import {Box} from "@chakra-ui/react";
+import { ReactNode } from 'react';
+import { Box } from '@chakra-ui/react';
 
 interface LayoutProps {
-    children: ReactNode;
+	children: ReactNode;
 }
-export const LayoutWrapper = ({children}: LayoutProps) => {
-    return(
-        <Box ml='1.5rem' mr='1.5rem' maxW='767px'>
-            {children}
-        </Box>
-    )
-}
+export const LayoutWrapper = ({ children }: LayoutProps) => {
+	return (
+		<Box ml="1.5rem" mr="1.5rem" maxW="767px">
+			{children}
+		</Box>
+	);
+};
