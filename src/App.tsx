4bbdc4e1d0f443ccf7ee3c7d373f4af61c182662
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-16 06:08:11 +0000

    ADD: Added home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './chakra.theme';
+import { theme } from './theme/chakra.theme';
 import NavigationBar from './components/NavigationBar';
+import { Home } from './components/Home';
 export default function App() {
 	return (
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 		<ChakraProvider theme={theme} resetCSS>
 			<NavigationBar />
+			<Home />
 		</ChakraProvider>
 	);
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,77 @@
+import { Box, Grid, Heading, Icon, Link, Text, Button } from '@chakra-ui/react';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import CustomButton from './Button';
+
+export const Home = () => {
+	return (
+		<Grid
+			as="section"
+			id="home"
+			layerStyle="sectionStyles"
+			gap={{ base: '1rem' }}
+		>
+			<Grid
+				rowGap={{ md: '5rem' }}
+				layerStyle="wrapper"
+				border="3px black solid"
+			>
+				<Grid
+					pt={{ base: '2.5rem', lg: '5.5rem' }}
+					columnGap={{ lg: 8 }}
+					alignItems="center"
+					gridTemplateColumns={{ base: '0.5fr 3fr', md: 'max-content 1fr 1fr' }}
+				>
+					<Grid
+						gridTemplateColumns="max-content"
+						rowGap={4}
+						alignItems="center"
+					>
+						<Link
+							color="brand.primary"
+							href="https://www.linkedin.com/in/nicholas-siaw-8824a6175/"
+							rel="noreferrer"
+							target="_blank"
+						>
+							{<FaLinkedin size="1.5rem" />}
+						</Link>
+						<Link
+							color="brand.primary"
+							href="https://www.twitter.com/ayequill"
+							target="_blank"
+							rel="noreferrer"
+						>
+							{<FaTwitter size="1.5rem" />}
+						</Link>
+						<Link
+							color="brand.primary"
+							href="http://github.com/ayequill"
+							target="_blank"
+							rel="noreferrer"
+						>
+							{<FaGithub size="1.5rem" />}
+						</Link>
+					</Grid>
+					<Box w="260px" h="260"></Box>
+					<Box gridColumn={{ base: '1/3', md: 'initial' }}>
+						<Heading>Siaw A. Nicholas</Heading>
+						<Text color="brand.text" mb={3.5}>
+							Frontend Developer
+						</Text>
+						<Text mb={8}>
+							High level experience in web design and development knowledge,
+							producing quality work.
+						</Text>
+						<CustomButton
+							variant="brandPrimary"
+							href="#contact"
+							label="Contact Me"
+						/>
+					</Box>
+				</Grid>
+				<Box>
+					<a href="#about">Scroll down</a>
+				</Box>
+			</Grid>
+		</Grid>
+	);
+};
