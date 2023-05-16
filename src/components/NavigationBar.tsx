e7013d1cad0bd58a9267e5e1d35c1abd0c11ee30
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-16 21:02:55 +0000

    FIX: Fix types

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, FC } from 'react';
 import {
 	FaBriefcase,
 	FaFileCode,
@@ -11,7 +11,7 @@ import {
 import { Box, Flex, IconButton, Link, Icon } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-const NavigationBar: React.FC = () => {
+const NavigationBar: FC = () => {
 	const [isOpen, setOpen] = useState(false);
 
 	const handleShowMenu = (): void => {
