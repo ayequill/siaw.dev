4e47ce580a8431384731aa2c87007a72c259b4a6
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-05-21 12:12:19 +0000

    ADD: Experience Data

---
+++ src/misc/experience.data.ts
@@ -0,0 +1,57 @@
+export const data: {
+	education: {
+		duration: string;
+		shift: boolean;
+		institute: string;
+		id: number;
+		title: string;
+	}[];
+	work: {
+		duration: string;
+		shift: boolean;
+		institute: string;
+		id: number;
+		title: string;
+	}[];
+} = {
+	education: [
+		{
+			duration: '2015 - 2019',
+			id: 1,
+			institute: 'DataLink University, Tema',
+			shift: false,
+			title: 'Computer Science',
+		},
+		{
+			id: 2,
+			title: 'Frontend Developer Career Path',
+			institute: 'Scrimba',
+			duration: '2022 - 2023',
+			shift: true,
+		},
+	],
+
+	work: [
+		{
+			id: 1,
+			title: 'Network Technician',
+			institute: 'Hardric Engineering, Accra',
+			duration: '2017 - 2018',
+			shift: true,
+		},
+		{
+			id: 2,
+			title: 'Network Technician',
+			institute: 'ComNet SA LTD, Accra',
+			duration: '2018 - Present',
+			shift: false,
+		},
+		{
+			id: 3,
+			title: 'Freelance',
+			institute: 'Delawin Farms',
+			duration: '2019 - 2020',
+			shift: true,
+		},
+	],
+};
