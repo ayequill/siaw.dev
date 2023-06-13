3562fbc8a74e1c75acf2ff327434ef3c8cbcaf02
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-06-13 18:36:30 +0000

    Added  new projects

--- src/misc/portfolio.data.ts
+++ src/misc/portfolio.data.ts
@@ -1,13 +1,19 @@
-import moviewWatch from '../assets/projects/screenshots/movie-desk.png';
+import moviewWatch from '../assets/projects/screenshots/movie-desk.webp';
 import movieMob from '../assets/projects/screenshots/movie-mob.png';
 import colorGenDesktop from '../assets/projects/screenshots/color-gen-desktop.png';
 import colorGenMobile from '../assets/projects/screenshots/color-gen-mobile.png';
 import tenziesGameDesktop from '../assets/projects/screenshots/tenzies-desk.png';
-import tenziesMobile from '../assets/projects/screenshots/tenzies-mob.png';
+import tenziesMobile from '../assets/projects/screenshots/tenzies-mob.webp';
 import markdownDesktop from '../assets/projects/screenshots/markdown-desk.png';
-import markdownMobile from '../assets/projects/screenshots/markdown-mob.png';
+import markdownMobile from '../assets/projects/screenshots/markdown-mob.webp';
 import dictionaryDesk from '../assets/projects/screenshots/dictionary-desk.png';
-import dictionaryMob from '../assets/projects/screenshots/dictionary-mob.png';
+import dictionaryMob from '../assets/projects/screenshots/dictionary-mob.webp';
+import booleBotDesktop from '../assets/projects/screenshots/boolebot-desktop.webp';
+import booleBotMob from '../assets/projects/screenshots/boolebot-mob.webp';
+import ipTrackerDesk from '../assets/projects/screenshots/ip-adress-tracker-desk.webp';
+import ipTrackerMob from '../assets/projects/screenshots/ip-adress-tracker-mobile.webp';
+import movieLibdesk from '../assets/projects/screenshots/movie-library-desktop.webp';
+import movieLibMob from '../assets/projects/screenshots/movie-library-mob.webp';
 
 interface PortfolioItem {
 	title: string;
@@ -21,8 +27,35 @@ interface PortfolioItem {
 
 export const portfolioData: PortfolioItem[] = [
 	{
-		title: 'Dictionary',
-		desc: 'A Dictionary web app built using the Open dictionary API.',
+		title: 'Boole Bots ',
+		desc: 'Boolebots aims to make learning boolean logic fun and engaging by allowing players to configure a series of bots and pit them against each other in battles. The outcome of each battle is determined by the principles of boolean logic, an essential concept in computer science and programming. By observing and analyzing the results, players can develop a deeper understanding of how boolean logic operates. Developed with my team at Chingu.',
+		demoUrl: 'https://boolebots.siaw.dev',
+		repoUrl: 'https://github.com/chingu-voyages/v44-tier2-team-27',
+		desktopImgSrc: booleBotDesktop,
+		mobileImgSrc: booleBotMob,
+		tags: ['react-ts', 'css', 'typescript'],
+	},
+	{
+		title: 'IP Address Tracker',
+		desc: 'This is a web application built using React and TypeScript that allows users to track the geographical location of an IP address. It utilizes the Leaflet library for interactive maps and makes API calls to IPify for retrieving IP address data.',
+		demoUrl: 'https://ip-tracker.siaw.dev',
+		repoUrl: 'https://github.com/ayequill/Ip-adress-tracker-with-react',
+		desktopImgSrc: ipTrackerDesk,
+		mobileImgSrc: ipTrackerMob,
+		tags: ['react-js', 'chakra-ui', 'leaflet-js', 'ipify'],
+	},
+	{
+		title: 'Movie Library WebApp',
+		desc: 'A movie collection Website adaptable to all devices, developed with TheMovieDB API.',
+		demoUrl: 'https://movie-lib.siaw.dev',
+		repoUrl: 'https://github.com/ayequill/movie-library',
+		desktopImgSrc: movieLibdesk,
+		mobileImgSrc: movieLibMob,
+		tags: ['react-js', 'chakra-ui', 'api'],
+	},
+	{
+		title: 'Dictionary WebApp',
+		desc: 'This is a simple dictionary webapp developed with React and Chakra UI. Users can search for words and get their definitions and synonyms.',
 		demoUrl: 'https://dic.siaw.dev',
 		repoUrl: 'https://github.com/ayequill/dictionary',
 		desktopImgSrc: dictionaryDesk,
