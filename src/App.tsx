e2049d99563f79f8928c6280233d030d007338b4
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-10 23:53:10 +0000

    FEAT:Removed components and added Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import About from './components/About';
-import { Home } from './components/Home';
+import Router from './Router';
 import NavigationBar from './components/NavigationBar';
-import Portfolio from './components/Portfolio';
-import { Qualification } from './components/Qualification';
-import { Skills } from './components/Skills';
 import { theme } from './theme/chakra.theme';
 import { SectionWrapper } from './components/SectionWrapper';
-import Project from './components/Project';
-import Contact from './components/ContactForm';
-import Footer from './components/Footer';
 export default function App() {
 	return (
 		<ChakraProvider theme={theme} resetCSS>
 			<NavigationBar />
 			<SectionWrapper>
-				<Home />
-				<About />
-				<Skills />
-				<Qualification />
-				<Portfolio />
-				<Project />
-				<Contact />
+				{/*	<Home />*/}
+				{/*	<About />*/}
+				{/*	<Skills />*/}
+				{/*	<Qualification />*/}
+				{/*	<Portfolio />*/}
+				{/*	<Project />*/}
+				{/*	<Contact />*/}
+				<Router />
 			</SectionWrapper>
-			<Footer />
+			{/*<Footer />*/}
 		</ChakraProvider>
 	);
 }
